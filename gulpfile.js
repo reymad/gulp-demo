49901c552bb4a27c2dd8ae3cb95b158bc3c93997
jrey <jrey@proximity.es>
2017-01-19 18:22:56 +0100

    sass tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,23 @@
 ﻿/*
 * Dependencias
 */
+/*
 var gulp = require('gulp'),
   concat = require('gulp-concat'),
   uglify = require('gulp-uglify');
-
+*/
 /*
 * Configuración de la tarea 'demo'
 */
+
+/*
 gulp.task('demo', function () {
   gulp.src('js/source/*.js')
   .pipe(concat('todo.js'))
   .pipe(uglify())
   .pipe(gulp.dest('js/build/'))
 });
+*/
 
 /*
  * watchers
@@ -24,18 +28,35 @@ var gulp = require('gulp'),
     jadeTasks = ['html:frontend'],
     coffeeTasks = ['js'],
     stylusTasks = ['styles'];
+*/
 
-path.watch = {
-    jade: ['jade/**/*.jade'],
-    stylus: ['stylus/**/*.styl'],
-    coffee: ['coffee/**/*.coffee']
-};
-
+//path.watch = {
+//    jade: ['jade/**/*.jade'],
+//    stylus: ['stylus/**/*.styl'],
+//    coffee: ['coffee/**/*.coffee']
+//};
+/*
 gulp.task('watch', function () {
 
     gulp.watch(path.watch.jade, jadeTasks);
     gulp.watch(path.watch.coffee, coffeeTasks);
     gulp.watch(path.watch.stylus, stylusTasks);
 });
-
 */
+
+
+//* watcher for sass
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
